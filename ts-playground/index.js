273b6fda68ad6bb330de82aeecc7bfe7c4f5085d
deliwalayash <deliwalayash2@gmail.com>
2025-12-29 19:01:03 +0530

    tyscript learning 1st commit

---
+++ ts-playground/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var message = "Hello TypeScript";
+console.log(message);
+var age = 25;
+// age="yash"
+age = 36;
+console.log(age);
+var username = "yash";
+var score = 35;
+var isLoggedIn = true;
+var arr = [1, 2, 3, 4, 5];
+arr.push(6);
+score = "high";
+console.log(score);
+console.log(arr);
---
+++ ts-playground/index.ts
@@ -0,0 +1,20 @@
+let message = "Hello TypeScript"
+console.log(message)
+let age = 25
+// age="yash"
+age = 36
+console.log(age)
+
+
+let username:string="yash"
+let score:number | string=35
+let isLoggedIn:boolean=true
+
+let arr:number[]=[1,2,3,4,5]
+let names:string[]=["a","b","c"]
+arr.push(6)
+
+score="high"
+console.log(score)
+console.log(arr)
+export {}
