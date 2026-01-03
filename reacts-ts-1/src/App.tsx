4476fa7158d77173321d88b2ef77446df8512861
deliwalayash <deliwalayash2@gmail.com>
2026-01-03 17:25:28 +0530

    spi calling in type script

--- reacts-ts-1/src/App.tsx
+++ reacts-ts-1/src/App.tsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+interface Userprops {
+name:string,
+age?:number
+}
+
+interface User {
+  name:string,
+  age:number
+}
+
+interface Apiuser{
+  id:number,
+  name:string,
+  email:string
+}
+
+
+const App = ({name,age}:Userprops) => {
+
+  const [count,setCount]=useState<number>(0)
+  const [count2,setCount2]=useState<string>("aaa")
+  const [user,setUser]=useState<User | null>()
+
+  const [user2,setUser2]=useState<Apiuser[]>([])
+
+  useEffect(()=>{
+
+    fetch("https://jsonplaceholder.typicode.com/users").then(res=>res.json()).then((data:Apiuser[])=>{setUser2(data)})
+  },[])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <div>
+      <h1>hello react ,how r u?</h1>
+      <h1>{count}</h1>
+      {user && <h2>{user.name}</h2>}
+      <h2></h2>
+      <h1>{count2}</h1>
+      <button onClick={()=>{setCount(count+1)}}>ADD</button>
+      <button onClick={()=>{setCount2("bbb")}}>ADD 2</button>
+      <h1>{name}</h1>
+      <h1>{age}</h1>
+      {user2 && user2.map((curEle)=>{
+        return(
+          <div key={curEle.id}>
+            <h1>{curEle.name}</h1>
+            <h2>{curEle.email}</h2>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
--- reacts-ts-1/src/main.tsx
+++ reacts-ts-1/src/main.tsx
@@ -5,6 +5,6 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App name="yash" age={35} />
   </StrictMode>,
 )
